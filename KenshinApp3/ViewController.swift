c299416beaed6dc80ea1b93a2f11c74c1c20d04f
InaShow <gagdev14@gmail.comls>
2018-11-06 17:04:21 +0900

    Initial Commit

---
+++ KenshinApp3/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  KenshinApp3
+//
+//  Created by 1490402 on 2018/11/06.
+//  Copyright © 2018年 GaGdev14. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ KenshinApp3Tests/KenshinApp3Tests.swift
@@ -0,0 +1,34 @@
+//
+//  KenshinApp3Tests.swift
+//  KenshinApp3Tests
+//
+//  Created by 1490402 on 2018/11/06.
+//  Copyright © 2018年 GaGdev14. All rights reserved.
+//
+
+import XCTest
+@testable import KenshinApp3
+
+class KenshinApp3Tests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
