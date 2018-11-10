3235f07e08e0507b0950cf8b5e8265f0df8019f0
InaShow <gagdev14@gmail.comls>
2018-11-10 18:23:56 +0900

    雛形となるタブバー追加

---
+++ KenshinApp3/VCCustomer.swift
@@ -0,0 +1,19 @@
+//
+//  VCCustmer.swift
+//  KenshinApp3
+//
+//  Created by 1490402 on 2018/11/10.
+//  Copyright © 2018年 GaGdev14. All rights reserved.
+//
+
+import UIKit
+
+class VCCustomer: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    
+}
