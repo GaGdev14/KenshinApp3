0e07dbca620e90a67c1a8d9952aa1156c320a3a4
maiooo2 <navy.blue.02mi@gmail.com>
2018-11-11 12:53:44 +0900

    詳細画面にSegmentでContainerViewの切り替えを実装。現状２画面。

---
+++ KenshinApp3/VCDetail.swift
@@ -0,0 +1,38 @@
+//
+//  VCDetail.swift
+//  KenshinApp3
+//
+//  Created by MaiInagaki on 2018/11/11.
+//  Copyright © 2018年 GaGdev14. All rights reserved.
+//
+
+import UIKit
+
+class VCDetail: UIViewController {
+ 
+    @IBOutlet weak var changeContauner: UISegmentedControl!
+    @IBOutlet weak var containerView: UIView!
+    @IBOutlet weak var serviceContainer: UIView!
+    @IBOutlet weak var dogContainer: UIView!
+    //@IBOutlet weak var otherContainer: UIView!
+    var containers: Array<UIView> = []
+    
+    //インデックス
+    //var num:Int = 0
+    let appDelegate = UIApplication.shared.delegate as! AppDelegate
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        containers = [serviceContainer,dogContainer]
+        containerView.bringSubviewToFront(serviceContainer)
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+    
+    @IBAction func changeContainerView(_ sender: UISegmentedControl) {
+        let currentContainerView = containers[sender.selectedSegmentIndex]
+        containerView.bringSubviewToFront(currentContainerView)
+    }
+}
