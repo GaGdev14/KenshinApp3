12a49b25a121df35c166d9513ab58dd361965fbc
maiooo2 <navy.blue.02mi@gmail.com>
2018-12-08 10:58:05 +0900

    スワイプ検知機能実装

--- KenshinApp3/VCDetail.swift
+++ KenshinApp3/VCDetail.swift
@@ -17,12 +17,41 @@ class VCDetail: UIViewController {
     @IBOutlet weak var otherContainer: UIView!
     var containers: Array<UIView> = []
     
+    // 実験。ここに何人目のお客さまかという情報を入れる予定
+    var count :Int = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         containers = [serviceContainer,dogContainer,otherContainer]
         containerView.bringSubviewToFront(serviceContainer)
+        
+        // 何人目のお客さまか表示
+        print(count)
+        
+        let rightSwipe = UISwipeGestureRecognizer(target: self, action: #selector(handleSwipe(sender:)))
+        rightSwipe.direction = .right
+        view.addGestureRecognizer(rightSwipe)
+        
+        let leftSwipe = UISwipeGestureRecognizer(target: self, action: #selector(handleSwipe(sender:)))
+        leftSwipe.direction = .left
+        view.addGestureRecognizer(leftSwipe)
     }
     
+    @objc final func handleSwipe(sender: UISwipeGestureRecognizer) {
+        
+        if sender.state == .ended {
+            switch sender.direction {
+            case .right:
+                print("前のお客さま")
+            case .left:
+                print("次のお客さま")
+            default:
+                break
+            }
+        }
+        
+    }
+
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
     }
