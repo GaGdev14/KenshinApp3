0c97461ba435a5950e24ecce67c7e46a29993356
maiooo2 <navy.blue.02mi@gmail.com>
2018-12-18 14:41:15 +0900

    Tabに画像追加。また、Tab選択時の色を茶色に変更

--- KenshinApp3/VCTabbarViewController.swift
+++ KenshinApp3/VCTabbarViewController.swift
@@ -14,6 +14,8 @@ class VCTabbarViewController: UITabBarController,UITabBarControllerDelegate{
     override func viewDidLoad() {
         super.viewDidLoad()
         self.delegate = self
+        
+        self.tabBar.tintColor = UIColor.brown
 
         // Do any additional setup after loading the view.
     }
